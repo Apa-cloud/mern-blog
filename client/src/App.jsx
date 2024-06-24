30979629a0b3b0de3cea33385ad479141e7e8c07
Apa-cloud <sircar.aparajita@gmail.com>
2024-06-24 22:14:14 +0530

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,33 @@
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+// imported the BrowserRouter for making the pages.
+// Now we no longer need the HTML h1, className, etc.
+// Everything will come under Routes and BrowserRouter now
+
 import React from 'react'
+import SignUp from './pages/SignUp'
+import SignIn from './pages/SignIn'
+import Home from './pages/Home'
+import Dashboard from './pages/Dashboard'
+import About from './pages/About'
+import Projects from './pages/Projects'
+
+
+
 
 export default function App() {
   return (
-    <h1 className='text-3xl text-red-500'> App</h1>
-  )
+    <BrowserRouter>
+    <Routes>
+      <Route path = "/" element={<Home />}/>
+      <Route path="/about" element={<About />}/>
+      <Route path="/sign-up" element={<SignUp />}/>
+      <Route path="/sign-in" element={<SignIn />}/>
+      <Route path="/dashboard" element={<Dashboard />} /> 
+      <Route path="/projects" element={<Projects />} />   
+      
+      </Routes>
+    
+    
+    </BrowserRouter>
+  );
 }
