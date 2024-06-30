0669379088b9de79fc1b42ce24a09459d8b12ed9
Apa-cloud <sircar.aparajita@gmail.com>
2024-06-30 09:31:17 +0530

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
 import express from 'express'; /* So we have changed "type" to module, in package.json in root directory */
+import mongoose from 'mongoose'; /* helps in easily interacting with mongoDB */
+import dotenv from 'dotenv'; /* So that we can use the .env file here in the backend */
+
+dotenv.config(); /* To configure the .env file */
+
+
+/* Connecting to database. Password has to be given */
+mongoose.connect(process.env.MONGO)
+.then(() => {
+    console.log('MongoDb is connected');  /* To make sure db is connected */
+}).catch(err => {
+    console.log(err);
+});
 
 const app = express()  /* Creating the application */
 
@@ -6,10 +19,17 @@ const app = express()  /* Creating the application */
 
 app.listen(3000, () => {      
 
-    console.log("Server is running on port 3000!");
+    console.log("Server is running on port 3000");
 
 });
 
 /* Installing nodemon so that we do not need to restart server to see the changes 
 Since we have changed the "scripts" in package.json, so while deploying, we need to write node api/index.js, not nodemon.
 So now we can simply run saying npm run dev */
+
+/* gitignore folder dragged to root directory so that unnecessary files do not get pushed */
+
+/* Stopped at MongoDB error MongoAPIError: URI must include hostname, domain name, and tld  */
+/* encoding for @ is    %40      */
+/* Connection string is used in .env file ; So that when we upload to Github, our password does not get exposed along with the connection string.
+The .env file added to gitignore so that it does not get uploaded to github */
