2fece65ba7e9fed2a9b928993ea77d9db429fd97
Apa-cloud <sircar.aparajita@gmail.com>
2024-07-03 15:52:23 +0530

    Added Delete user api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { test, updateUser } from '../controllers/user.controller.js';
+import { deleteUser, test, updateUser } from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
 router.get('/test', test);  /* We have imported this test from contoller */
 router.put('/update/:userId', verifyToken,updateUser);   // For UPDATING User // /update/{userID copied from database}
+router.delete('/delete/:userId', verifyToken, deleteUser);
 
 export default router; /* Since we are using default, so we can import this using any name  */
