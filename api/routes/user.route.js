79ad98f0fd5e80e6b5f859f45c20679bd4d24a9e
Apa-cloud <sircar.aparajita@gmail.com>
2024-07-03 16:47:25 +0530

    added signout functionaility

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { deleteUser, test, updateUser } from '../controllers/user.controller.js';
+import { deleteUser, signout, test, updateUser } from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.get('/test', test);  /* We have imported this test from contoller */
 router.put('/update/:userId', verifyToken,updateUser);   // For UPDATING User // /update/{userID copied from database}
 router.delete('/delete/:userId', verifyToken, deleteUser);
+router.post('/signout', signout);
 
 export default router; /* Since we are using default, so we can import this using any name  */
