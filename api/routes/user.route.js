f0f736dce101a8f9d44968384500478cb634d0d5
Apa-cloud <sircar.aparajita@gmail.com>
2024-07-04 15:46:30 +0530

    added delete user functionality to the admin

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -8,6 +8,6 @@ router.get('/test', test);  /* We have imported this test from contoller */
 router.put('/update/:userId', verifyToken,updateUser);   // For UPDATING User // /update/{userID copied from database}
 router.delete('/delete/:userId', verifyToken, deleteUser);
 router.post('/signout', signout);
-
 router.get('/getusers', verifyToken, getUsers);
+
 export default router; /* Since we are using default, so we can import this using any name  */
